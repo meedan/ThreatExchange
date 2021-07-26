1c44491ccba467a9dad69dc376163d35c2a1c5ce
computermacgyver <computermacgyver@users.noreply.github.com>
2021-07-26 17:03:09 +0100

    update minimal_example_query

--- tmk/swig/minimal_example_query.py
+++ tmk/swig/minimal_example_query.py
@@ -1,11 +1,27 @@
 import tmkpy
 import glob
+from datetime import datetime
+import random
+
 vid=tmkpy.hashVideo("../sample-videos/chair-19-sd-bar.mp4","/usr/bin/ffmpeg")
 
 haystack=[f for f in glob.glob("../sample-hashes/*.tmk")]
+#haystack=[item for item in haystack for i in range(50)] #Make the list 50 copies every file
+#random.shuffle(haystack)
 
-scores=tmkpy.query(vid,haystack,1)
 
-scores=[(x,y) for x,y in zip(haystack,scores)]
-print(scores)
+start=datetime.now()
+scores=None
+try:
+	scores=tmkpy.query(vid,haystack,1)
+except RuntimeError as e:
+	print(e)
+
+end=datetime.now()
+
+print(end-start)
+
+if scores!=None:
+	for file,score in zip(haystack,scores):
+		print(file,score)
 
